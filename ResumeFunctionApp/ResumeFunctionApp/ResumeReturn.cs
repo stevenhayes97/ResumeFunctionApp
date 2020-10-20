a7470604b036d130a92892ad966ac92b0e9d7150
Steven Hayes <shayes2@mmm.com>
2020-10-19 23:49:30 -0500

    Added Function App

---
+++ ResumeFunctionApp/ResumeFunctionApp/ResumeReturn.cs
@@ -0,0 +1,25 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace ResumeFunctionApp
+{
+    public static class ResumeReturn
+    {
+        [FunctionName("ResumeReturn")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            string responseMessage = "Function App Call OK";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
